a9e5cd48c58e40d25a030ed22d57d72077d98f97
Nikolay Kurtov <nkurtov@gmail.com>
2011-10-14 22:14:16 +0200

    main.js smth

--- javascript/main.js
+++ javascript/main.js
@@ -5,9 +5,9 @@ var gamejs = require('gamejs');
 gamejs.ready(function() {
 
     var display = gamejs.display.setMode([800, 600]);
-    var room = new Room();
+    var room = new Room(5);
     display.blit(
-        (new gamejs.font.Font('30px Sans-serif')).render(room.width + ' and ' +  room.height)
+        (new gamejs.font.Font('30px Sans-serif')).render(room.get(2,1)+ ' and ' +  room.get(1,1))
     );
 
     /**
