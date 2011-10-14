51da1051b3bad539dbefbadcd050585be6801be9
Nikolay Kurtov <nkurtov@gmail.com>
2011-10-14 21:44:36 +0200

    room with getters

--- javascript/room.js
+++ javascript/room.js
@@ -1,7 +1,19 @@
+var ROOM_HEIGHT = 9;
+var ROOM_WIDTH = 7;
 
-function Room() {
-  this.width = 9;
-  this.height = 7;
-  this.data = new Array(this.width * this.height);
+function Room(id) {
+  var _id = id;
+  var _state = new Array();
+  for (var i = 0; i < ROOM_HEIGHT; i++) {
+    _state[i] = new Array();
+    for (var j = 0; j < ROOM_WIDTH; j++) {
+      _state[i][j] = 0;
+    }
+  }
+  this.id = function() {
+    return _id;
+  }
+  this.get = function(x,y) {
+    return _state[y][x];
+  }
 }
-
