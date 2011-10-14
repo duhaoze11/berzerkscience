376368905f6662cd6885097859e9f47fe7624cde
Nikolay Kurtov <nkurtov@gmail.com>
2011-10-14 22:40:50 +0200

    merge 2

--- javascript/main.js
+++ javascript/main.js
@@ -2,8 +2,6 @@ var gamejs = require('gamejs');
 
 // $gamejs.preload([]);
 
-<<<<<<< HEAD
-=======
 function draw(display, room) {
   for (var i=0; i<ROOM_WIDTH; i++) {
     for (var j=0; j<ROOM_HEIGHT; j++) {
@@ -16,14 +14,11 @@ function draw(display, room) {
         (new gamejs.font.Font('30px Sans-serif')).render(ROOM_HEIGHT)
     );*/
 
->>>>>>> bb25e8fc3c818eea019d073460cbffd278f2cfad
 gamejs.ready(function() {
 
     var display = gamejs.display.setMode([800, 600]);
     var room = new Room(5);
-    display.blit(
-        (new gamejs.font.Font('30px Sans-serif')).render(room.id())
-    );
+    draw(display, room);
 
     /**
     function tick() {
