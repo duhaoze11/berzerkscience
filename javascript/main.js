3526cba415d791632f8a59912eb8f1cee844834d
Nikolay Kurtov <nkurtov@gmail.com>
2011-10-14 22:35:01 +0200

    map stuff

--- javascript/main.js
+++ javascript/main.js
@@ -2,17 +2,13 @@ var gamejs = require('gamejs');
 
 // $gamejs.preload([]);
 
-function draw(display, room) {
-    display.blit(
-        (new gamejs.font.Font('30px Sans-serif')).render(room.id())
-    );
-}
-
 gamejs.ready(function() {
 
     var display = gamejs.display.setMode([800, 600]);
-    var room = new Room(4);
-    draw(display, room);
+    var room = new Room(5);
+    display.blit(
+        (new gamejs.font.Font('30px Sans-serif')).render(room.id())
+    );
 
     /**
     function tick() {
@@ -22,4 +18,3 @@ gamejs.ready(function() {
     gamejs.time.fpsCallback(tick, this, 26);
     **/
 });
-
--- javascript/room.js
+++ javascript/room.js
@@ -2,19 +2,21 @@ var ROOM_HEIGHT = 9;
 var ROOM_WIDTH = 7;
 
 function Room(id) {
-  var _id = id;
-  var _state = new Array();
+  this._id = id;
+  this._state = new Array();
   for (var i = 0; i < ROOM_HEIGHT; i++) {
-    _state[i] = new Array();
+    this._state[i] = new Array();
     for (var j = 0; j < ROOM_WIDTH; j++) {
-      _state[i][j] = 0;
+      this._state[i][j] = 0;
     }
   }
-  this.id = function() {
-    return _id;
-  }
-  this.get = function(x,y) {
-    return _state[y][x];
-  }
+}
+
+Room.prototype.id = function() {
+  return this._id;
+}
+
+Room.prototype.get = function(x,y) {
+  return this._state[y][x];
 }
 
